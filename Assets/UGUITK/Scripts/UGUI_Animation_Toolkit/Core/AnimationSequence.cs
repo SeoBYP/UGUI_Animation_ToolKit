1ff80a4b4c804489b1388b93a1caf75041bd5e4e
Jack-SparrowInKorea <dandansoftware@gmail.com>
2025-06-29 18:41:48 +0900

    Reapply "통합 Animator 구현중"
    This reverts commit 9d56bd08c60683d77078afc4425bc75e4f062be9.

---
+++ Assets/UGUITK/Scripts/UGUI_Animation_Toolkit/Core/AnimationSequence.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace UGUIAnimationToolkit.Core
+{
+    [Serializable]
+    public class AnimationSequence
+    {
+        [Tooltip("모듈 실행 방식 (동시 또는 순차)")] public ExecutionMode mode = ExecutionMode.Parallel;
+
+        [SerializeReference] public List<UIAnimationModule> modules = new List<UIAnimationModule>();
+        private readonly List<UIAnimationModule> _executeModules = new List<UIAnimationModule>();
+
+        public async UniTask PlayAsync(UIAnimationContext ctx)
+        {
+            ctx.MotionHandle.Complete();
+            _executeModules.Clear();
+
+            switch (mode)
+            {
+                case ExecutionMode.Parallel:
+                {
+                    var tasks = new List<UniTask>();
+                    foreach (var module in modules)
+                    {
+                        if (module is null || !module.Enable)
+                            continue;
+                        _executeModules.Add(module);
+                        tasks.Add(ExecuteModuleWithDelay(module, ctx));
+                    }
+                    await UniTask.WhenAll(tasks);
+                }
+                    break;
+                case ExecutionMode.Sequential:
+                {
+                    foreach (var module in modules)
+                    {
+                        if (module is null || !module.Enable)
+                            continue;
+                        _executeModules.Add(module);
+                        await ExecuteModuleWithDelay(module, ctx);
+                    }
+                }
+                    break;
+            }
+        }
+
+        public async UniTask RevertAsync(UIAnimationContext ctx)
+        {
+            ctx.MotionHandle.Complete();
+            var tasks = new List<UniTask>();
+            foreach (var module in _executeModules)
+            {
+                if (module is null || !module.Enable)
+                    continue;
+                tasks.Add(module.RevertAsync(ctx));
+            }
+            await UniTask.WhenAll(tasks);
+        }
+
+        private async UniTask ExecuteModuleWithDelay(UIAnimationModule module, UIAnimationContext ctx)
+        {
+            if (module.Delay > 0)
+            {
+                await UniTask.Delay(TimeSpan.FromSeconds(module.Delay), ignoreTimeScale: true);
+            }
+
+            await module.AnimateAsync(ctx);
+        }
+    }
+}
---
+++ Assets/UGUITK/Scripts/UGUI_Animation_Toolkit/Editor/Core/UIAnimationEventDrawer.cs
@@ -0,0 +1,69 @@
+﻿using UGUIAnimationToolkit.Core;
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+namespace UGUIAnimationToolkit.Eidtor.Core
+{
+    [CustomPropertyDrawer(typeof(UIAnimationEvent))]
+    public class UIAnimationEventDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var eventNameProp = property.FindPropertyRelative("eventName");
+
+            // 전체를 box로 감싸줍니다.
+            EditorGUI.BeginProperty(position, label, eventNameProp);
+            var boxRect = new Rect(position.x, position.y, position.width, GetPropertyHeight(property, label) - 4);
+            GUI.Box(boxRect, GUIContent.none, EditorStyles.helpBox);
+
+            // Foldout 헤더
+            var headerRect = new Rect(position.x, position.y + 2, position.width, EditorGUIUtility.singleLineHeight);
+            property.isExpanded = EditorGUI.Foldout(headerRect, property.isExpanded,
+                string.IsNullOrEmpty(eventNameProp.stringValue) ? "New Event" : eventNameProp.stringValue, true,
+                EditorStyles.boldLabel);
+
+            if (property.isExpanded)
+            {
+                var contentPosition = new Rect(position.x + 15, position.y + EditorGUIUtility.singleLineHeight + 4, position.width - 20, position.height);
+                var currentY = contentPosition.y;
+                // Event Name
+                var eventNameRect = new Rect(contentPosition.x, currentY, contentPosition.width, EditorGUIUtility.singleLineHeight);
+                EditorGUI.PropertyField(eventNameRect, eventNameProp, new GUIContent("Event Name"));
+                currentY += EditorGUI.GetPropertyHeight(eventNameProp) + 4;
+                
+                // Sequence
+                var sequenceProp = property.FindPropertyRelative("sequence");
+                var sequenceRect = new Rect(contentPosition.x, currentY, contentPosition.width,EditorGUI.GetPropertyHeight(sequenceProp,true));
+                EditorGUI.PropertyField(eventNameRect, sequenceProp, new GUIContent("Sequence"),true);
+                currentY += sequenceRect.height + 4;
+                
+                // On Completed
+                var onCompletedProp = property.FindPropertyRelative("onCompleted");
+                var onCompletedRect = new Rect(contentPosition.x, currentY, contentPosition.width,EditorGUI.GetPropertyHeight(onCompletedProp,true));
+                EditorGUI.PropertyField(onCompletedRect, onCompletedProp, new GUIContent("On Completed"));
+            }
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            if (!property.isExpanded)
+            {
+                return EditorGUIUtility.singleLineHeight + 4;
+            }
+            
+            var eventNameProp = property.FindPropertyRelative("eventName");
+            var sequenceProp = property.FindPropertyRelative("sequence");
+            var onCompletedProp = property.FindPropertyRelative("onCompleted");
+            
+            float height = EditorGUIUtility.singleLineHeight + 8;
+            height += EditorGUI.GetPropertyHeight(eventNameProp, true) + 4;
+            height += EditorGUI.GetPropertyHeight(sequenceProp, true) + 4;
+            height += EditorGUI.GetPropertyHeight(onCompletedProp, true) + 4;
+
+            return height;
+        }
+    }
+}
+#endif
